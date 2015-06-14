65c2c1cf14c20b3145fe5912cbcc4fc851509084
Liam Magee <liam.magee@gmail.com>
2015-06-14 11:37:10 +1000

    Updated dbpedia queries

--- R/dbpedia.R
+++ R/dbpedia.R
@@ -7,10 +7,27 @@ library(SPARQL)
 # Define the data.gov endpoint
 endpoint <- "http://dbpedia.org/sparql/"
 
-# helper functions
-concat <- function(pre, part, post) {
-  return (paste0(pre, part, post))
+## helper functions
+
+# Construct the query
+constructQuery <- function(query_parts) {
+  location <- gsub(" ", "_", query_parts[1])
+  return (c(paste0(query_parts[2], location, query_parts[3]), query_parts[1]))
 }
+
+# Execute the query
+doQuery <- function(query) {
+  qd <- SPARQL(url = endpoint, query = query[1])
+  df <- data.frame(qd$results)
+  if ( length(df) > 0 )
+    df$location <- query[2]
+  return (df)
+}
+
+# Read locations table
+
+suburbnames = read.table("data/westernsydney_suburbnames.txt", sep=",", header = TRUE, row.names = 1)
+suburbnames <- rbind(suburbnames, "Mount Druitt")
 
 # create query statement
 query_pt1 <- "
@@ -20,16 +37,18 @@ SELECT ?p, ?o WHERE { <http://dbpedia.org/resource/"
 query_pt2 <- "> ?p ?o . }"
 
 # Entities
-locations <- c("Mount_Druitt", "")
-query_parts <- data.frame(locations)
+#query_parts <- data.frame(suburbnames$x[33])
+query_parts <- data.frame(suburbnames)
 query_parts[, "pt1"] <- (query_pt1)
 query_parts[, "pt2"] <- (query_pt2)
-apply(locations, 1, concat) 
-paste0(query_pt1, , query_pt2)
+queries <- apply(query_parts, 1, constructQuery) 
 
 # Step 2 - Use SPARQL package to submit query and save results to a data frame
-qd <- SPARQL(url = endpoint, query = query)
-df <- qd$results
-df$p
-df$o
-
+results <- apply(queries, 2, doQuery)
+merged_data <- data.frame(location=character(), p=character(), o=character(), stringsAsFactors=FALSE)
+for (row in results) {
+  df <- data.frame(row)
+  df$location
+  merged_data <- rbind(merged_data, df)
+}
+print(results)
