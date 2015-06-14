c4fd5824479753b7f6fb26201c67ee7f586e9ec8
Liam Magee <liam.magee@gmail.com>
2015-06-14 10:11:09 +1000

    Update queries

--- R/dbpedia.R
+++ R/dbpedia.R
@@ -7,12 +7,25 @@ library(SPARQL)
 # Define the data.gov endpoint
 endpoint <- "http://dbpedia.org/sparql/"
 
+# helper functions
+concat <- function(pre, part, post) {
+  return (paste0(pre, part, post))
+}
+
 # create query statement
-query <-"
+query_pt1 <- "
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema/>
-SELECT ?p, ?o WHERE { <http://dbpedia.org/resource/Malabaine> ?p ?o . }
-"
+SELECT ?p, ?o WHERE { <http://dbpedia.org/resource/"
+query_pt2 <- "> ?p ?o . }"
+
+# Entities
+locations <- c("Mount_Druitt", "")
+query_parts <- data.frame(locations)
+query_parts[, "pt1"] <- (query_pt1)
+query_parts[, "pt2"] <- (query_pt2)
+apply(locations, 1, concat) 
+paste0(query_pt1, , query_pt2)
 
 # Step 2 - Use SPARQL package to submit query and save results to a data frame
 qd <- SPARQL(url = endpoint, query = query)
